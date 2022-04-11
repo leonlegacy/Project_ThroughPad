ff34a0dd9ec469748fc190002f3727ee993300b3
hans0930 <s83526100@gmail.com>
2022-04-11 20:07:23 +0800

    upup

---
+++ Assets/ipadBinding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ipadBinding : MonoBehaviour
+{
+    [SerializeField] Transform ipad;
+
+    private void Start()
+    {
+        StartCoroutine(bind());
+    }
+
+    IEnumerator bind()
+    {
+        yield return new WaitForSeconds(1);
+        ipad.parent = transform;
+        //ipad.localPosition = Vector3.zero;
+    }
+}

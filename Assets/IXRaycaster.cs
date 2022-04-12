62dc1ff26c38374af4e78dc4ab5f1142a8bd7423
Leon <bsexp357079@gmail.com>
2022-04-12 16:22:16 +0800

    Raycaster

---
+++ Assets/IXRaycaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IXRaycaster : MonoBehaviour
+{
+    [SerializeField] Transform ipad;
+    [SerializeField] LayerMask ignoreLayer;
+    private void Update()
+    {
+        transform.LookAt(ipad);
+    }
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, ~ignoreLayer))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow, 3);
+            Debug.Log("Hit " + hit.transform.name);
+        }
+    }
+}
---
+++ Assets/LaserTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTest : MonoBehaviour
+{
+    // Event callback example: Debug-draw all contact points and normals for 2 seconds.
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.green, 2, false);
+    }
+}

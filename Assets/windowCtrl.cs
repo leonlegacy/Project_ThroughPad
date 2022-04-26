59ad7862b35fa844fab075fa5104fbcf8172a969
hans0930 <s83526100@gmail.com>
2022-04-26 15:59:25 +0800

    ghhhff

---
+++ Assets/windowCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowCtrl : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    bool isOpen = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isOpen = !isOpen;
+            anim.SetBool("open", isOpen);
+        }
+    }
+}
